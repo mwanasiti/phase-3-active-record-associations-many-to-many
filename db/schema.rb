a108134a39c8a134df9f0ed1b08049a7e7386cbb
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-12 22:54:37 +0300

    commit tree 22ed04342c76e69523a373f264088ac4f0da8e49

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2022_09_12_131257) do
     t.integer "game_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
